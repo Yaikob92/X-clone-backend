b6f8ee3583073824d410febffd67c672d8ffa6f5
Yaikob/92 <wyaikob@gmail.com>
2025-11-19 18:30:49 +0300

    database connection and models setup

--- src/server.js
+++ src/server.js
@@ -1,5 +1,24 @@
 import express from "express";
+import { ENV } from "./config/env.js";
+import { connect } from "mongoose";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 
-app.listen(5001, () => console.log("Server is running on port 5001"));
+app.get("/", (req, res) => {
+  res.send("Hello from server");
+});
+
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(ENV.PORT, () =>
+      console.log("Server is up adn running on PORT:", ENV.PORT)
+    );
+  } catch (error) {
+    console.error("Failed to start server:", error.message);
+    process.exit(1);
+  }
+};
+startServer();
